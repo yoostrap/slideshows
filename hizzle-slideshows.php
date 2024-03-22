744e31dc1aa00686da0872c98aa6a52ee050ae8a
Lewis Ushindi <frenziecodes@gmail.com>
2024-03-22 16:24:02 +0300

    define plugin constants

--- hizzle-slideshows.php
+++ hizzle-slideshows.php
@@ -32,11 +32,29 @@ add_action( 'init', 'hizzle_slideshows_load_textdomain' );
 function hizzle_slideshows_register_block() {
 
 	// Register the block by passing the location of block.json to register_block_type.
-	register_block_type( plugin_dir_path( __FILE__ ) . 'build/slideshow' );
-    register_block_type( plugin_dir_path( __FILE__ ) . 'build/slide' );
+	register_block_type( HSS_PATH . 'build/slideshow' );
+    register_block_type( HSS_PATH . 'build/slide' );
 
 	if ( function_exists( 'wp_set_script_translations' ) ) {
 		wp_set_script_translations( 'hizzle-slideshows-slideshow', 'hizzle-slideshows' );
 	}
 }
 add_action( 'init', 'hizzle_slideshows_register_block' );
+
+// Define some essentials constants.
+
+if ( !defined( 'HSS_SLUG' ) ) {
+    define( 'HSS_SLUG', 'hizzle-slideshows' );
+    define( 'HSS_HANDLE', 'hizzle-slideshows' );
+    define( 'HSS_DIR', __DIR__ );
+    define( 'HSS_PATH', plugin_dir_path( __FILE__ ) );
+    define( 'HSS_URL', plugin_dir_url( __FILE__ ) );
+    define( 'HSS_ASSETS_URL', HSS_URL . 'assets/' );
+    define( 'HSS_FILE', __FILE__ );
+    define( 'HSS_VERSION', '1.0.1' );
+    define( 'HSS_MIN_PHP', '7.0.0' );
+    define( 'HSS_MIN_WP', '6.0.0' );
+}
+
+// Include the includes file.
+require_once HSS_PATH . 'includes/includes.php';
